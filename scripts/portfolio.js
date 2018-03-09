b61b49cc8b05a272016f183c653be85f74dbc351
Ssuarez0 <ssuarez@2011.nhi.edu>
2018-03-08 18:29:44 -0800

    Final Update: Scrolling
    Scrolling has been optimized and animated. All styles are done and this
    can be considered the final product barring the addition of more
    projects to the portfolio or new skills.

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -1,2 +1,9 @@
 ﻿$(document).ready(function () {
+    $('a.scrollable').click(function (e) {
+        e.preventDefault();
+        var $anchorHref = $(this).attr('href');
+        $elementId = $($anchorHref);
+        /* Both html and body necessary to accomodate all browsers. */
+        $('html, body').stop().animate({ scrollTop: $($elementId).offset().top }, 1400);
+    });
 });
