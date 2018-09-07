a08df4dfdc3972706224cf0af15e265ffd85d424
Ssuarez0 <ssuarez@2011.nhi.edu>
2018-09-06 18:12:12 -0700

    Portfolio 2.0
    React slideshow is done with the exception of two additional features.
    Eventually, I'll want to add a maximize button for the images and a
    opactiy transition for the slide descriptions. However, for now this
    works.

--- scripts/src/portfolio.js
+++ scripts/src/portfolio.js
@@ -16,86 +16,105 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 var currentProjects = [{
     key: 0,
     name: "Revaliir.net",
-    description: "A medium size forum website serving a dedicated, writing community. </br><strong>Live technology stack:</strong> Django 1.8, python 2.7, jquery, and bootstrap 3. </br><strong>In development technology stack:</strong> React.js, Node.js, Graphene, Django 2.0+, Python 3.7+",
+    description: "A medium sized, forum website serving a dedicated, writing community. </br><strong>Live technology stack:</strong> Django 1.8, python 2.7, jquery, and Bootstrap. </br><strong>In development technology stack:</strong> React.js, Node.js, Graphene, Django 2.0+, Python 3.7+",
     projectImg: "./media/images/Revaliir.PNG",
-    githubLink: "https://github.com/Ssuarez0/",
+    githubLink: "",
     websiteLink: "https://revaliir.net/index/"
 }, {
     key: 1,
+    name: "Pomodoro Clock",
+    description: "An alternating clock to help users keep track of taking regular breaks. </br><strong>Technology stack:</strong> ES6, React.js, SCSS, Bootstrap.",
+    projectImg: "./media/images/PomodoroClock.png",
+    githubLink: "",
+    codepenLink: "https://codepen.io/CodeAni/pen/Zjojdr",
+    websiteLink: "https://codepen.io/CodeAni/full/Zjojdr/"
+}, {
+    key: 2,
+    name: "React Calculator",
+    description: "A simple calculator built using React. </br><strong>Technology stack:</strong> ES6, React.js, SCSS, Bootstrap.",
+    projectImg: "./media/images/ReactCalculator.PNG",
+    githubLink: "",
+    codepenLink: "https://codepen.io/CodeAni/pen/JBNjYr",
+    websiteLink: "https://codepen.io/CodeAni/full/JBNjYr/"
+}, {
+    key: 3,
+    name: "Drum Machine",
+    description: "A 'drum machine' that allows the user to play 9 different notes from a piano by either clicking buttons or utilizing the keyboard. </br><strong>Technology stack:</strong> ES6, React.js, SCSS, Bootstrap",
+    projectImg: "./media/images/DrumMachine.PNG",
+    githubLink: "",
+    codepenLink: "https://codepen.io/CodeAni/pen/RBPmLV",
+    websiteLink: "https://codepen.io/CodeAni/full/RBPmLV"
+}, {
+    key: 4,
+    name: "Markdown Previewer",
+    description: "A markdown previewer that is actually the alpha of a custom markdown editor. This reads markdown syntax in a textbox and renders it as regular html. </br><strong>Technology stack:</strong> ES6, React.js, SCSS, Bootstrap, Markdown.js",
+    projectImg: "./media/images/MarkdownPreviewer.PNG",
+    githubLink: "",
+    codepenLink: "https://codepen.io/CodeAni/pen/OwPqoJ",
+    websiteLink: "https://codepen.io/CodeAni/full/OwPqoJ"
+}, {
+    key: 5,
+    name: "Random Quote Machine",
+    description: "A very simple react project that uses fetch to acquire a json object of quotes and then choses one at random that is appropriate for twitter. </br><strong>Technology stack:</strong> ES6, React.js, SCSS, Bootstrap, Fetch.js",
+    projectImg: "./media/images/RandomQuote.PNG",
+    githubLink: "",
+    codepenLink: "https://codepen.io/CodeAni/pen/RBbNKg",
+    websiteLink: "https://codepen.io/CodeAni/full/RBbNKg"
+}, {
+    key: 6,
     name: "Spanish Frequency Analyzer",
     description: "A desktop application built for a high school Spanish teacher to help determine the frequency at which words appear in lecture notes. </br><strong>Technology stack:</strong> C# and WPF.",
     projectImg: "./media/images/spanishFrequency.PNG",
-    githubLink: "https://github.com/Ssuarez0/",
-    websiteLink: ""
-}, {
-    key: 2,
-    name: "Test project",
-    description: "",
-    projectImg: "./media/images/Revaliir.PNG",
-    githubLink: "https://github.com/Ssuarez0/",
-    websiteLink: ""
-}, {
-    key: 3,
-    name: "Test project",
-    description: "",
-    projectImg: "./media/images/Revaliir.PNG",
-    githubLink: "https://github.com/Ssuarez0/",
-    websiteLink: ""
-}, {
-    key: 4,
-    name: "Test project",
-    description: "",
-    projectImg: "./media/images/Revaliir.PNG",
-    githubLink: "https://github.com/Ssuarez0/",
-    websiteLink: ""
-}, {
-    key: 5,
-    name: "Test project",
-    description: "",
-    projectImg: "./media/images/Revaliir.PNG",
-    githubLink: "https://github.com/Ssuarez0/",
-    websiteLink: ""
-}, {
-    key: 6,
-    name: "Test project",
-    description: "",
-    projectImg: "./media/images/Revaliir.PNG",
-    githubLink: "https://github.com/Ssuarez0/",
+    githubLink: "https://github.com/Ssuarez0/spanish-nl-analyzer",
+    codepenLink: "",
     websiteLink: ""
 }, {
     key: 7,
-    name: "Test project",
-    description: "",
-    projectImg: "./media/images/Revaliir.PNG",
-    githubLink: "https://github.com/Ssuarez0/",
-    websiteLink: ""
+    name: "CS-Club-Hackathon Registration Page 2017",
+    description: "A registration page for a 2017 Hackathon hosted by the CSU Stanislaus CS Club and sponsored by Novo Consulting. </br><strong>Technology stack:</strong> LAMP stack project with MySQL, PHP, and a Linux server.",
+    projectImg: "./media/images/CS-Club-Hackathon.PNG",
+    githubLink: "https://github.com/Ssuarez0/CS-Club-Hackathon",
+    codepenLink: "",
+    websiteLink: "https://hopper.csustan.edu/~ssuarez/cs-club-hackathon/index.html"
 }, {
     key: 8,
-    name: "Test project",
-    description: "",
-    projectImg: "./media/images/Revaliir.PNG",
-    githubLink: "https://github.com/Ssuarez0/",
+    name: "Stanislaus Bird Encyclopedia",
+    description: "A website built to list all the birds in Stanislaus county in alphabetical order and then supply individual pages full of details for each bird. </br><strong>Technology stack:</strong> Django 1.10 and python 3.5.",
+    projectImg: "./media/images/BirdDatabase.PNG",
+    githubLink: "https://github.com/Ssuarez0/CS4950---Bird-Encyclopedia",
+    codepenLink: "",
     websiteLink: ""
 }, {
     key: 9,
-    name: "Test project",
-    description: "",
-    projectImg: "./media/images/Revaliir.PNG",
-    githubLink: "https://github.com/Ssuarez0/",
+    name: "Battle Arena",
+    description: "A battle arena game with a fully functioning AI operating on a modified Negamax algorithm.</br><strong>Technology stack:</strong> Fully done in python 3.5 on Windows",
+    projectImg: "./media/images/BattleArenaSample.PNG",
+    githubLink: "https://github.com/Ssuarez0/CS4480---Battle-Arena-AI",
+    codepenLink: "",
     websiteLink: ""
 }, {
     key: 10,
-    name: "Test project",
-    description: "",
-    projectImg: "./media/images/Revaliir.PNG",
-    githubLink: "https://github.com/Ssuarez0/",
-    websiteLink: ""
+    name: "Sample Biography Page",
+    description: "A sample website of a fictional character's life. </br><strong>Technology stack:</strong> HTML and Bootstrap.",
+    projectImg: "./media/images/SampleBiography.PNG",
+    githubLink: "https://github.com/Ssuarez0/AaronWhiteBiographyPage",
+    codepenLink: "https://codepen.io/CodeAni/pen/qpzBJQ",
+    websiteLink: "https://codepen.io/CodeAni/full/qpzBJQ"
 }, {
     key: 11,
-    name: "Test project",
-    description: "",
-    projectImg: "./media/images/Revaliir.PNG",
-    githubLink: "https://github.com/Ssuarez0/",
+    name: "Library Database Website",
+    description: "A website for querying a hypothetical library. </br><strong>Technology stack:</strong> LAMP stack project with MySQL, PHP, and a Linux server.",
+    projectImg: "./media/images/LibraryDatabase.PNG",
+    githubLink: "https://github.com/Ssuarez0/CS4250-Library-Database",
+    codepenLink: "",
+    websiteLink: ""
+}, {
+    key: 12,
+    name: "Bee Pesticide Simulation",
+    description: "A simulation meant to demonstrate the interaction between polination, bees, pesticides and the food supply. </br><strong>Technology stack:</strong> Netlogo",
+    projectImg: "./media/images/BeePesticideModel.PNG",
+    githubLink: "https://github.com/Ssuarez0/CS3200-Bee-Pesticide-Simulation",
+    codepenLink: "",
     websiteLink: ""
 }];
 
@@ -224,13 +243,18 @@ var SlideDescription = function (_React$Component3) {
             var links = [];
             if (currentSlide.websiteLink && currentSlide.websiteLink.length > 0) links.push(React.createElement(
                 "a",
-                { key: "website-link", className: "project-link", title: "Website", target: "_blank", href: currentSlide.websiteLink },
+                { key: "website-link", className: "project-link", title: "Live Website", target: "_blank", href: currentSlide.websiteLink },
                 React.createElement("i", { className: "fas fa-globe" })
             ));
             if (currentSlide.githubLink && currentSlide.githubLink.length > 0) links.push(React.createElement(
                 "a",
                 { key: "github-link", className: "project-link", title: "Github", target: "_blank", href: currentSlide.githubLink },
                 React.createElement("i", { className: "fab fa-github" })
+            ));
+            if (currentSlide.codepenLink && currentSlide.codepenLink.length > 0) links.push(React.createElement(
+                "a",
+                { key: "codepen-link", className: "project-link", title: "CodePen", target: "_blank", href: currentSlide.codepenLink },
+                React.createElement("i", { "class": "fab fa-codepen" })
             ));
 
             return React.createElement(
